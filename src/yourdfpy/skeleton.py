80b74c1b3f2553f58a08a52144972107126689ad
Clemens Eppner <ceppner@nvidia.com>
2021-11-06 22:40:56 -0700

    Skeleton update

--- src/yourdfpy/skeleton.py
+++ src/yourdfpy/skeleton.py
@@ -1,20 +1,5 @@
 """
-This is a skeleton file that can serve as a starting point for a Python
-console script. To run this script uncomment the following lines in the
-``[options.entry_points]`` section in ``setup.cfg``::
-
-    console_scripts =
-         fibonacci = yourdfpy.skeleton:run
-
-Then run ``pip install .`` (or ``pip install -e .`` for editable mode)
-which will install the command ``fibonacci`` inside your current environment.
-
-Besides console scripts, the header (i.e. until ``_logger``...) of this file can
-also be used as template for Python modules.
-
-References:
-    - https://setuptools.readthedocs.io/en/latest/userguide/entry_point.html
-    - https://pip.pypa.io/en/stable/reference/pip_install
+Script for visualizing a robot from a URDF.
 """
 
 import argparse
@@ -29,35 +14,6 @@ __copyright__ = "Clemens Eppner"
 __license__ = "MIT"
 
 _logger = logging.getLogger(__name__)
-
-
-# ---- Python API ----
-# The functions defined in this section can be imported by users in their
-# Python scripts/interactive interpreter, e.g. via
-# `from yourdfpy.skeleton import fib`,
-# when using this Python module as a library.
-
-
-def fib(n):
-    """Fibonacci example function
-
-    Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
-    """
-    assert n > 0
-    a, b = 1, 1
-    for _i in range(n - 1):
-        a, b = b, a + b
-    return a
-
-
-# ---- CLI ----
-# The functions defined in this section are wrappers around the main Python
-# API allowing them to be called directly from the terminal as a CLI
-# executable/script.
 
 
 def parse_args(args):
@@ -112,7 +68,7 @@ def parse_args(args):
 
 
 def setup_logging(loglevel):
-    """Setup basic logging
+    """Setup basic logging.
 
     Args:
       loglevel (int): minimum loglevel for emitting messages
@@ -124,10 +80,7 @@ def setup_logging(loglevel):
 
 
 def main(args):
-    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
-
-    Instead of returning the value from :func:`fib`, it prints the result to the
-    ``stdout`` in a nicely formatted message.
+    """Wrapper allowing string arguments in a CLI fashion.
 
     Args:
       args (List[str]): command line parameters as list of strings
@@ -135,16 +88,12 @@ def main(args):
     """
     args = parse_args(args)
     setup_logging(args.loglevel)
-    _logger.debug("Starting crazy calculations...")
-    
-    urdf_model = URDF.from_xml_file(args.input)
-    urdf_model.get_scene(configuration=args.configuration, use_collision_geometry=args.collision).show()
-
-    _logger.info("Script ends here")
+    urdf_model = URDF.load(args.input)
+    urdf_model.show(use_collision_geometry=args.collision)
 
 
 def run():
-    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`.
 
     This function can be used as entry point to create console scripts with setuptools.
     """
@@ -152,14 +101,4 @@ def run():
 
 
 if __name__ == "__main__":
-    # ^  This is a guard statement that will prevent the following code from
-    #    being executed in the case someone imports this file instead of
-    #    executing it as a script.
-    #    https://docs.python.org/3/library/__main__.html
-
-    # After installing your project with pip, users can also run your Python
-    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
-    #
-    #     python -m yourdfpy.skeleton 42
-    #
     run()
