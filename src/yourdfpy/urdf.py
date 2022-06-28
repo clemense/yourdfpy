43789f26dc8319088cbf08f60f2d7b9eb7baaae1
Clemens Eppner <clemens.eppner@gmail.com>
2022-06-28 09:10:17 -0700

    Add bug fix and test for single link URDF (#26)
    * Add bug fix and test for single link URDF
    
    * replace log.warn with log.warning

--- src/yourdfpy/urdf.py
+++ src/yourdfpy/urdf.py
@@ -1313,8 +1313,8 @@ class URDF:
             s.graph.update(frame_from=j.parent, frame_to=j.child, matrix=matrix)
 
         for l in self.robot.links:
-            if l.name not in s.graph.nodes:
-                _logger.warn(
+            if l.name not in s.graph.nodes and l.name != s.graph.base_frame:
+                _logger.warning(
                     f"{l.name} not connected via joints. Will add link to base frame."
                 )
                 s.graph.update(frame_from=s.graph.base_frame, frame_to=l.name)
